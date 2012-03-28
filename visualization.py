2a828810dc2cb9ed1cf05cfede1b91cf21a6ae5a
Joachim Nitschke <6nitschk@informatik.uni-hamburg.de>
2012-03-28 17:09:49 +0200

    visualization now has a global color scale that doesn't change from step to step

--- visualization.py
+++ visualization.py
@@ -41,16 +41,16 @@ class Visualization(object):
     ATTRIBUTE_KEY_COORDS = 3
 
     # Modes:
-    # COMPONENTS - display connected components
-    # USAGE      - display relative traffic load
-    # AMOUNT     - display absolute traffic load
-    # MAXSPEED   - display local speed limits
+    # COMPONENTS   - display connected components
+    # TRAFFIC_LOAD - display absolute traffic load
+    # MAX_SPEED    - display local speed limits
+    # ACTUAL_SPEED - display calculated actual speed based on traffic load
 
     # Colors:
-    # HEATMAP    - vary hue on a temperature-inspired scale from dark blue to red
-    # MONOCHROME - vary brightness from black to white
+    # HEATMAP      - vary hue on a temperature-inspired scale from dark blue to red
+    # MONOCHROME   - vary brightness from black to white
 
-    def __init__(self, street_network_filename_pattern, traffic_load_filename_pattern, mode = 'USAGE', color_mode = 'HEATMAP'):
+    def __init__(self, street_network_filename_pattern, traffic_load_filename_pattern, mode = 'TRAFFIC_LOAD', color_mode = 'HEATMAP'):
         print "Welcome to Streets4MPI visualization!"
         print "Current display mode:", mode, "with color mode", color_mode
 
@@ -59,7 +59,6 @@ class Visualization(object):
         self.coord2km = (111.32, 66.4) # distances between 2 deg of lat/lon
         self.bounds = None
         self.street_network = None
-        self.traffic_load = None
         self.node_coords = dict()
         self.mode = mode
         self.color_mode = color_mode
@@ -72,14 +71,34 @@ class Visualization(object):
         street_network_files = filter(self.street_network_filename_expression.search, all_files)
         traffic_load_files = filter(self.traffic_load_filename_expression.search, all_files)
 
+        # read street networks
+        street_networks = dict()
+        for street_network_file in street_network_files:
+            street_network = persist_read(street_network_file)
+            street_networks[street_network_file] = street_network
+
+        # keep max traffic laod to setup legend later
+        max_load = 0
+
+        # read traffic loads
+        traffic_loads = dict()
+        for traffic_load_file in traffic_load_files:
+            traffic_load = persist_read(traffic_load_file)
+            traffic_loads[traffic_load_file] = traffic_load
+
+            # find max traffic load
+            max_load = max(max_load, self.find_max_value(traffic_load))
+
         step = 0
-        while len(street_network_files + traffic_load_files) > 0:
+        while len(traffic_loads.keys()) > 0:
             step += 1
             print "Step counter", step
 
-            if "street_network_" + str(step) + ".s4mpi" in street_network_files:
+            # check if there is a street network for the current step and load it
+            street_network_filename = "street_network_" + str(step) + ".s4mpi"
+            if street_network_filename in street_networks.keys():
                 print "  Found street network data, reading..."
-                self.street_network = persist_read("street_network_" + str(step) + ".s4mpi")
+                self.street_network = street_networks[street_network_filename]
                 self.bounds = self.street_network.bounds
                 self.zoom = self.max_resolution[0] / max((self.bounds[0][1] - self.bounds[0][0]) * self.coord2km[0],
                                   (self.bounds[1][1] - self.bounds[1][0]) * self.coord2km[1])
@@ -94,31 +113,24 @@ class Visualization(object):
                 if self.mode == 'COMPONENTS':
                       self.calculate_components(self.street_network._graph)
 
-                street_network_files.remove("street_network_"+str(step)+".s4mpi")
-
-            if "traffic_load_"+str(step)+".s4mpi" in traffic_load_files:
+            # check if there is traffic load for the current step and draw it
+            traffic_load_filename = "traffic_load_" + str(step) + ".s4mpi"
+            if traffic_load_filename in traffic_loads.keys():
                 print "  Found traffic load data, reading and drawing..."
-                self.traffic_load = persist_read("traffic_load_"+str(step)+".s4mpi")
-                self.street_network_im = Image.new("RGBA", self.max_resolution, (0, 0, 0, 255))
-                draw = ImageDraw.Draw(self.street_network_im)
-
-                if self.mode == 'AMOUNT':
-                    max_amount = self.find_max_amount()
-                if self.mode == 'USAGE':
-                    max_usage = self.find_max_usage()
+                traffic_load = traffic_loads[traffic_load_filename]
+                street_network_image = Image.new("RGBA", self.max_resolution, (0, 0, 0, 255))
+                draw = ImageDraw.Draw(street_network_image)
 
                 for street, length, max_speed in self.street_network:
                     color = (255, 255, 255, 0) # default: white
                     width = max_speed / 50
                     value = 0
                     current_traffic_load = 0
-                    if street in self.traffic_load.keys():
-                        current_traffic_load = self.traffic_load[street]
-                    if self.mode == 'AMOUNT':
-                        value = 1.0 * current_traffic_load / max_amount
-                    if self.mode == 'USAGE':
-                        value = min(1.0, 5 * (1.0 * current_traffic_load) / (max_speed * max_usage))
-                    if self.mode == 'MAXSPEED':
+                    if street in traffic_load.keys():
+                        current_traffic_load = traffic_load[street]
+                    if self.mode == 'TRAFFIC_LOAD':
+                        value = 1.0 * current_traffic_load / max_load
+                    if self.mode == 'MAX_SPEED':
                         value = min(1.0, 1.0 * max_speed / 140)
                     color = self.value_to_color(value)
                     if self.mode == 'COMPONENTS':
@@ -126,11 +138,12 @@ class Visualization(object):
                         color = "hsl(" + str(int(137.5*component) % 360) + ",100%,50%)"
                     draw.line([self.node_coords[street[0]], self.node_coords[street[1]]], fill=color, width=width)
 
-                self.image_finalize()
-                print "  Saving image to disk (traffic_load_"+str(step)+".png) ..."
-                self.street_network_im.save("traffic_load_"+str(step)+".png")
+                self.image_finalize(street_network_image, max_load)
+                print "  Saving image to disk (traffic_load_" + str(step) + ".png) ..."
+                street_network_image.save("traffic_load_" + str(step) + ".png")
 
-                traffic_load_files.remove("traffic_load_"+str(step)+".s4mpi")
+                # remove current traffic load from dictionary
+                del traffic_loads[traffic_load_filename]
 
         print "Done!"
 
@@ -139,19 +152,11 @@ class Visualization(object):
         for edge in graph.edges():
             graph.add_edge_attribute(edge, (Visualization.ATTRIBUTE_KEY_COMPONENT, max(components[edge[0]], components[edge[1]])))
 
-    def find_max_amount(self):
-        amount = 0
-        for street, length, max_speed in self.street_network:
-            if street in self.traffic_load.keys():
-                amount = max(amount, 1.0 * self.traffic_load[street])
-        return amount
-
-    def find_max_usage(self):
-        usage = 0
-        for street, length, max_speed in self.street_network:
-            if street in self.traffic_load.keys():
-                usage = max(usage, 1.0 * self.traffic_load[street] / max_speed)
-        return usage
+    def find_max_value(self, dictionary):
+        max_value = 0
+        for value in dictionary.values():
+            max_value = max(max_value, 1.0 * value)
+        return max_value
 
     def value_to_color(self, value):
         value = min(1.0, max(0.0, value))
@@ -164,14 +169,14 @@ class Visualization(object):
             else: # blue to red
                 return "hsl(" + str(int(260*(1-(value-0.2)/0.8))) + ",100%,50%)"
 
-    def image_finalize(self):
+    def image_finalize(self, street_network_image, max_load):
         # take the current street network and make it pretty
         self.street_network_im = self.auto_crop(self.street_network_im)
 
         white = (255,255,255,0)
         black = (0,0,0,0)
         padding = self.max_resolution[0] / 40
-        legend = Image.new("RGBA", self.street_network_im.size, (0,0,0,255))
+        legend = Image.new("RGBA", street_network_image.size, (0,0,0,255))
         font = ImageFont.load_default()
         draw = ImageDraw.Draw(legend)
         bar_outer_width = self.max_resolution[0] / 50
@@ -180,7 +185,7 @@ class Visualization(object):
         bar_inner_width = bar_inner_width - (bar_outer_width - bar_inner_width) % 4
         bar_offset = max(2, int(bar_outer_width - bar_inner_width) / 2)
 
-        if self.mode in ['USAGE', 'AMOUNT', 'MAXSPEED']:
+        if self.mode in ['TRAFFIC_LOAD', 'MAXSPEED']:
             draw.rectangle([(0, 0), (bar_outer_width, legend.size[1]-1)], fill = white)
             border_width = int(bar_offset / 2)
             draw.rectangle([(border_width, border_width), (bar_outer_width-border_width, legend.size[1]-1-border_width)], fill = black)
@@ -188,13 +193,10 @@ class Visualization(object):
                 value = 1.0 * (y - bar_offset) / (legend.size[1] - 2 * bar_offset)
                 color = self.value_to_color(1.0 - value) # highest value at the top
                 draw.line([(bar_offset, y), (bar_offset + bar_inner_width, y)], fill=color)
-            if self.mode == 'AMOUNT':
-                top_text = str(round(self.find_max_amount(), 1))+ " cars gone through"
+            if self.mode == 'TRAFFIC_LOAD':
+                top_text = str(round(max_load, 1)) + " cars gone through"
                 bottom_text = "0 cars gone through"
-            if self.mode == 'USAGE':
-                top_text = str(round(self.find_max_usage(), 1))+ " car density (cars per allowed km/h)"
-                bottom_text = "0 car density (cars per allowed km/h)"
-            if self.mode == 'MAXSPEED':
+            if self.mode == 'MAX_SPEED':
                 top_text = "speed limit: 140 km/h or higher"
                 bottom_text = "speed limit: 0 km/h"
             draw.text((int(bar_outer_width * 1.3), 0), top_text, font = font, fill = white)
@@ -206,11 +208,11 @@ class Visualization(object):
         copyright = "Generated by Streets4MPI version 0.1 using data from the OpenStreetMap project. Licensed under CC-BY-SA 2.0 (https://creativecommons.org/licenses/by-sa/2.0/)."
         copyright_size = draw.textsize(copyright, font = font)
 
-        final_width = self.street_network_im.size[0] + legend.size[0] + 3 * padding
+        final_width = street_network_image.size[0] + legend.size[0] + 3 * padding
         final_height = legend.size[1] + 2 * padding + copyright_size[1] + 1
         final = Image.new("RGB", (final_width, final_height), black)
-        final.paste(self.street_network_im, (padding, padding))
-        final.paste(legend, (self.street_network_im.size[0] + 2 * padding, padding))
+        final.paste(street_network_image, (padding, padding))
+        final.paste(legend, (street_network_image.size[0] + 2 * padding, padding))
         ImageDraw.Draw(final).text((2, legend.size[1] + 2 * padding), copyright, font = font, fill = white)
 
         self.street_network_im = final
